91cd84d56cdaaff47d66ad42213bfb408305a92c
BlueManiac <BlueManiac@users.noreply.github.com>
2020-09-06 20:22:37 +0200

    Update set-git-repository-author.ps1

--- set-git-repository-author.ps1
+++ set-git-repository-author.ps1
@@ -1,4 +1,14 @@
+# Last commit
 git config user.name "BlueManiac"
 git config user.email "bluemaniac@users.noreply.github.com"
 git commit --amend --reset-author --no-edit
 git push -f
+
+# Older commits
+git config user.name "BlueManiac"
+git config user.email "bluemaniac@users.noreply.github.com"
+git checkout commitid
+git commit --amend --reset-author --no-edit
+git checkout branchname
+git rebase -i newcommitid
+git push --force-with-lease
