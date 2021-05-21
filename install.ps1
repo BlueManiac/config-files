25a0ea3922cae09885ae225ea53e967b5526b440
BlueManiac <BlueManiac@users.noreply.github.com>
2021-05-21 11:35:29 +0200

    Added Office365

--- install.ps1
+++ install.ps1
@@ -66,6 +66,11 @@ choco install -y visualstudio2019-workload-netcoretools
 choco install -y visualstudio2019-workload-nativedesktop
 
 
+# Office 365
+# Installs Excel, Powerpoint, Word, OneDrive, OneNote, Outlook
+choco install -y office365business /exclude:"Access Groove Lync Publisher" /updates:"FALSE" /eula:"TRUE"
+
+
 # Install powershell modules
 Install-PackageProvider -Name NuGet -Force
 Install-Module -Name SqlServer -Force # For Invoke-SqlCmd
