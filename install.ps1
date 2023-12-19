087b0b39bb58e32b7017715f1a11e492a2ba43f3
BlueManiac <bluemaniac@users.noreply.github.com>
2023-12-19 18:48:26 +0100

    Updated install script.

--- install.ps1
+++ install.ps1
@@ -1,148 +1,125 @@
-# List all local packages
-# choco list -l
-
 # Require admin rights
 If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
 {
-    $wshell = New-Object -ComObject Wscript.Shell
-	$wshell.Popup("You do not have Administrator rights to run this script!`nPlease re-run this script as an Administrator!", 0, "Warning", 0x1)
-	
-	break
+  $wshell = New-Object -ComObject Wscript.Shell
+  $wshell.Popup("You do not have Administrator rights to run this script!`nPlease re-run this script as an Administrator!", 0, "Warning", 0x1)
+  break
 }
 
-# Setup Chocolatey
-iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
+# APPLICATIONS
 
+# Microsoft store apps
+winget install --exact --source msstore --id 9P07XNM5CHP0 --accept-package-agreements # Ambie
+winget install --exact --source msstore --id 9P7KNL5RWT25 --accept-package-agreements # Sysinternals Suite
 
-# Install applications
-choco install -y 7zip
-choco install -y autohotkey
-choco install -y autoruns
-choco install -y azure-cli
-choco install -y beyondcompare --version 3.3.13.18981
-choco pin add --name beyondcompare --version 3.3.13.18981
-choco install -y ccleaner
-choco install -y discord
-choco install -y docker-desktop
-choco install -y git
-choco install -y google-backup-and-sync
-choco install -y googlechrome
-choco install -y instanteyedropper
-choco install -y irfanview
-choco install -y jre8
-choco install -y keepass
-choco install -y linqpad
-choco install -y microsoft-teams
-choco install -y microsoft-windows-terminal --pre 
-choco install -y microsoftazurestorageexplorer
-choco install -y mRemoteNG
-choco install -y sql-server-management-studio
-choco install -y nodejs
-choco install -y notepadplusplus
-choco install -y paint.net
-choco install -y postman
-choco install -y rufus
-choco install -y speccy
-choco install -y spotify
-choco install -y steam
-choco install -y sumatrapdf
-choco install -y teamviewer --version 13.2.5287
-choco pin add --name teamviewer --version 13.2.5287
-choco install -y tightvnc --install-arguments 'ADDLOCAL="Viewer" VIEWER_ASSOCIATE_VNC_EXTENSION=1 VIEWER_ADD_FIREWALL_EXCEPTION=1'
-#choco install -y virtualbox
-choco install -y VisualStudioCode
-choco install -y vlc
-choco install -y vscode
-choco install -y windirstat
+# Machine apps
+winget install --exact --source winget --scope machine --id 7zip.7zip
+winget install --exact --source winget --scope machine --id Brave.Brave
+#winget install --exact --source winget --scope machine --id Docker.DockerDesktop
+#winget install --exact --source winget --scope machine --id DominikReichl.KeePass
+winget install --exact --source winget --scope machine --id Git.Git
+winget install --exact --source winget --scope machine --id GitHub.cli
+winget install --exact --source winget --scope machine --id LINQPad.LINQPad.7
+winget install --exact --source winget --scope machine --id Microsoft.AzureCLI
+winget install --exact --source winget --scope machine --id Microsoft.PowerToys
+#winget install --exact --source winget --scope machine --id Microsoft.SQLServerManagementStudio
+winget install --exact --source winget --scope machine --id Microsoft.VisualStudioCode
+winget install --exact --source winget --scope machine --id Notepad++.Notepad++
+winget install --exact --source winget --scope machine --id OpenJS.NodeJS
+#winget install --exact --source winget --scope machine --id Oracle.JavaRuntimeEnvironment
+winget install --exact --source winget --scope machine --id Rufus.Rufus
+#winget install --exact --source winget --scope machine --id ScooterSoftware.BeyondCompare4
+winget install --exact --source winget --scope machine --id TeamViewer.TeamViewer
+winget install --exact --source winget --scope machine --id VideoLAN.VLC
+winget install --exact --source winget --scope machine --id WinDirStat.WinDirStat
+winget install --exact --source winget --scope machine --id dotPDNLLC.paintdotnet
+winget install --exact --source winget --scope machine --id mRemoteNG.mRemoteNG
+winget install --exact --source winget --scope machine --id pnpm
 
+# User apps
+winget install --exact --source winget --scope user --id Discord.Discord
+winget install --exact --source winget --scope user --id Microsoft.AzureDataStudio
+winget install --exact --source winget --scope user --id Microsoft.WindowsTerminal
+winget install --exact --source winget --scope user --id Spotify.Spotify # Can't be installed in admin console
 
-# Visual studio
-# See https://chocolatey.org/packages/visualstudio2019enterprise for other development packages
-choco install -y visualstudio2019enterprise
-choco install -y visualstudio2019-workload-manageddesktop
-choco install -y visualstudio2019-workload-netweb
-choco install -y visualstudio2019-workload-netcoretools
-choco install -y visualstudio2019-workload-nativedesktop
+# Visual studio, Workloads: https://docs.microsoft.com/en-us/visualstudio/install/workload-component-id-vs-enterprise?view=vs-2022&preserve-view=true
+winget install --exact --source winget --id Microsoft.VisualStudio.2022.Enterprise --override "--wait --passive --add Microsoft.VisualStudio.Workload.NetWeb --add Microsoft.VisualStudio.Workload.ManagedDesktop"
 
-
-# Office 365
-# Installs Excel, Powerpoint, Word, OneDrive, OneNote, Outlook
-# choco install -y office365business /exclude:"Access Groove Lync Publisher" /updates:"FALSE" /eula:"TRUE"
-
+# Reload enviroment variables
+$Env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine")
 
 # Install powershell modules
-Install-PackageProvider -Name NuGet -Force
 Install-Module -Name SqlServer -Force # For Invoke-SqlCmd
-refreshenv
 
+# Windows features
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All
+
+# dotnet tools
+dotnet tool install --global dotnet-outdated-tool
+dotnet tool install --global dotnet-format
+
+# TWEAKS
 
 # Show file extensions
 Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name HideFileExt -Value 0
 
-# Set UAC to never notify
-Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name PromptOnSecureDesktop -Value 0
-Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name EnableLUA -Value 1
-Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name ConsentPromptBehaviorAdmin -Value 0
-
-# Disable Cortana
-Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search" -Name SearchboxTaskbarMode -Value 0 -Force
-
-# Enable developer mode in windows 10
+# Enable developer mode in windows 10/11
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\AppModelUnlock" -Name AllowDevelopmentWithoutDevLicense -Value 1
 
-# Remove task view button in taskbar
-Set-ItemProperty -Force -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "ShowTaskViewButton" -Value 0 
+# Use old context menu in windows 11
+New-Item -Force -Path "HKCU:\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}" -Name "InprocServer32"
+Set-ItemProperty -Force -Path "HKCU:\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" -Name "(Default)" -Value ''
+
+# Disable OneDrive personal account (use business only)
+Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\OneDrive" -Name DisablePersonalSync -Value 1
+
+# Set the default action of the power button on the Start menu to Shut Down
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer" -Name PowerButtonAction -Value 2
+
+# Hide the search bar
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Search" -Name SearchboxTaskbarMode -Value 0
+
+# Hide the task view button
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name ShowTaskViewButton -Value 0
+
+# Hide the widgets button
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name TaskbarDa -Value 0
+
+# Hide the chat button
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name TaskbarMn -Value 0
 
 
-# Remove Shortcuts on desktop
-Get-ChildItem -Path "$Env:SystemDir\Users\Public\Desktop" *.lnk | foreach { Remove-Item -Path $_.FullName }
-Get-ChildItem -Path "$Env:UserProfile\Desktop" *.lnk | foreach { Remove-Item -Path $_.FullName }
-
+# VARIOUS
 
 # Url to github repository
 $githubUrl = 'https://raw.githubusercontent.com/BlueManiac/config-files/master'
-
-
-# Replace notepad with notepad++
-$filePath = 'C:\Program Files\Notepad++\Notepad++ParamProxy.exe'
-Invoke-WebRequest -Uri "$githubUrl/Notepad++/Notepad++ParamProxy.exe" -OutFile $filePath
-New-Item -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options" -Name "notepad.exe" -Force
-Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\notepad.exe" -Name Debugger -Value """$filePath""" -Force
-
 
 # Copy .gitconfig
 $filePath = "$Env:UserProfile\.gitconfig"
 Invoke-WebRequest -Uri "$githubUrl/.gitconfig" -OutFile $filePath
 
-
-# Add nuget sources
-dotnet nuget add source https://api.nuget.org/v3/index.json -n nuget.org
-
-
 # Create directories
 New-Item -ItemType directory -ErrorAction SilentlyContinue -Path 'C:\Private'
 New-Item -ItemType directory -ErrorAction SilentlyContinue -Path 'C:\Repositories'
 
+# Remove Shortcuts on desktop
+Get-ChildItem -Path "$Env:SystemDir\Users\Public\Desktop" *.lnk | foreach { Remove-Item -Path $_.FullName }
+Get-ChildItem -Path "$Env:UserProfile\Desktop" *.lnk | foreach { Remove-Item -Path $_.FullName }
 
 # Add Windows Defender exclusions
 Add-MpPreference -ExclusionPath 'C:\Private'
-Add-MpPreference -ExclusionPath 'C:\Repositories'
-Add-MpPreference -ExclusionPath 'C:\Program Files (x86)\Microsoft Visual Studio'
 Add-MpPreference -ExclusionPath 'C:\Program Files (x86)\MSBuild'
+Add-MpPreference -ExclusionPath 'C:\Program Files (x86)\Microsoft Visual Studio'
+Add-MpPreference -ExclusionPath 'C:\Program Files (x86)\Steam\steamapps\common'
+Add-MpPreference -ExclusionPath 'C:\Program Files\MSBuild'
+Add-MpPreference -ExclusionPath 'C:\Program Files\Microsoft Visual Studio'
+Add-MpPreference -ExclusionPath 'C:\Repositories'
+Add-MpPreference -ExclusionProcess 'ServiceHub.RoslynCodeAnalysisService.exe'
 Add-MpPreference -ExclusionProcess 'devenv.exe'
 Add-MpPreference -ExclusionProcess 'dotnet.exe'
 Add-MpPreference -ExclusionProcess 'msbuild.exe'
-
-
-# Disable Chrome Software Reporter Tool from eating my CPU
-New-Item -Path "HKLM:\SOFTWARE\Policies\Google" -Name "Chrome" -Force
-Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Google\Chrome" -Name ChromeCleanupEnabled -Value 0
-Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Google\Chrome" -Name ChromeCleanupReportingEnabled -Value 0
-
-
-# Disable OneDrive personal account (use business only)
-Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\OneDrive" -Name DisablePersonalSync -Value 1
-
+Add-MpPreference -ExclusionProcess 'node.exe'
+Add-MpPreference -ExclusionProcess 'npm.exe'
 
 Write-Host "Press any key to continue ..."
 $x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
